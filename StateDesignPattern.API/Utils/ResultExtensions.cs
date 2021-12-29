d925709ac2d93dc75e140fbf6dbb158020e5c78c
Bernhard Maier <mail@bernhard-maier.net>
2021-12-29 15:34:14 +0100

    Add generalized method for failure to action result conversion

--- StateDesignPattern.API/Utils/ResultExtensions.cs
+++ StateDesignPattern.API/Utils/ResultExtensions.cs
@@ -10,42 +10,37 @@ namespace StateDesignPattern.API.Utils;
 
 public static class ResultExtensions
 {
+  private static ActionResult ConvertFailureToActionResult(string error) =>
+    Enum.TryParse(error, out HttpStatusCode statusCode)
+      ? new StatusCodeResult((int) statusCode)
+      : new BadRequestObjectResult(error);
+
   public static ActionResult EnvelopeAsOk(this Result result) =>
     result.IsSuccess
       ? new OkResult()
-      : Enum.TryParse(result.Error, out HttpStatusCode statusCode)
-        ? new StatusCodeResult((int) statusCode)
-        : new BadRequestObjectResult(result.Error);
+      : ConvertFailureToActionResult(result.Error);
 
   public static ActionResult EnvelopeAsOk<T>(this Result<T> result) =>
     result.IsSuccess
       ? new OkResult()
-      : Enum.TryParse(result.Error, out HttpStatusCode statusCode)
-        ? new StatusCodeResult((int) statusCode)
-        : new BadRequestObjectResult(result.Error);
+      : ConvertFailureToActionResult(result.Error);
 
   public static ActionResult<T> EnvelopeAsOkObject<T>(this Result<T> result) =>
     result.IsSuccess
       ? new OkObjectResult(result.Value)
-      : Enum.TryParse(result.Error, out HttpStatusCode statusCode)
-        ? new StatusCodeResult((int) statusCode)
-        : new BadRequestObjectResult(result.Error);
+      : ConvertFailureToActionResult(result.Error);
 
   public static ActionResult<T> EnvelopeAsCreated<T>(this Result<T> result)
     where T : IHasGuid =>
     result.IsSuccess
       ? new CreatedResult(result.Value.Id.ToString(), result.Value)
-      : Enum.TryParse(result.Error, out HttpStatusCode statusCode)
-        ? new StatusCodeResult((int) statusCode)
-        : new BadRequestObjectResult(result.Error);
+      : ConvertFailureToActionResult(result.Error);
   
   public static ActionResult EnvelopeWithRoute<T>(this Result<T> result, string targetRouteName)
     where T : IHasGuid =>
     result.IsSuccess
       ? new CreatedAtRouteResult(targetRouteName, new { id = result.Value.Id }, null)
-      : Enum.TryParse(result.Error, out HttpStatusCode statusCode)
-        ? new StatusCodeResult((int) statusCode)
-        : new BadRequestObjectResult(result.Error);
+      : ConvertFailureToActionResult(result.Error);
 
   public static async Task<ActionResult> EnvelopeWithRoute<T>(this Task<Result<T>> resultTask, string targetRouteName)
     where T : IHasGuid =>
@@ -55,9 +50,7 @@ public static class ResultExtensions
     result.IsSuccess
       ? new FileContentResult(result.Value.File, MediaTypeNames.Application.Pdf)
         { FileDownloadName = result.Value.Name }
-      : Enum.TryParse(result.Error, out HttpStatusCode statusCode)
-        ? new StatusCodeResult((int) statusCode)
-        : new BadRequestObjectResult(result.Error);
+      : ConvertFailureToActionResult(result.Error);
 
   public static async Task<ActionResult> EnvelopeWithPdf(this Task<Result<(byte[], string)>> resultTask) =>
     (await resultTask).EnvelopeWithPdf();
