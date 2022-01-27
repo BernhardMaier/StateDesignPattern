b70e23222d1550a0f1d29cff3b334722b13da5c5
Bernhard Maier <mail@bernhard-maier.net>
2022-01-27 20:22:43 +0100

    Activate swagger ui in release mode

--- StateDesignPattern.API/Startup.cs
+++ StateDesignPattern.API/Startup.cs
@@ -29,7 +29,9 @@ public class Startup
   // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
   public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
   {
-    if (env.IsDevelopment())
+    const bool activateSwagger = true; //env.IsDevelopment();
+    
+    if (activateSwagger)
     {
       app.UseDeveloperExceptionPage();
       app.UseSwagger();
